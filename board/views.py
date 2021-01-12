ab0b77f60cbab9b8c8ac18b0a94cc90937619302
N0FreeLunch <consilience90@gmail.com>
2021-01-12 23:38:19 +0900

    use fomr

--- board/views.py
+++ board/views.py
@@ -1,6 +1,50 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
 from django.http import HttpResponse
+from .models import Question
+from django.utils import timezone
+from .forms import QuestionForm
 
 # Create your views here.
 def index(request):
-    return HttpResponse("안녕하세요 pybo에 오신것을 환영합니다.")
+    question_list = Question.objects.order_by('-create_date')
+    context = {'question_list': question_list}
+    # return HttpResponse("안녕하세요 pybo에 오신것을 환영합니다.")
+    return render(request, 'board/question_list.html', context)
+
+def detail(request, question_id):
+    """
+    pybo 내용 출력
+    """
+    # question = Question.objects.get(id=question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    context = {'question': question}
+    return render(request, 'board/question_detail.html', context)
+
+def answer_create(request, question_id):
+    """
+    pybo 답변등록
+    """
+    question = get_object_or_404(Question, pk=question_id)
+    question.answer_set.create(content=request.POST.get('content'), create_date=timezone.now())
+    return redirect('board:detail', question_id=question.id)
+
+def question_create(request):
+    """
+    pybo 질문등록
+    """
+
+    if request.method == 'POST':
+        form = QuestionForm(request.POST)
+        print(request.POST["subject"])
+        print(request.POST["content"])
+        if form.is_valid():
+            question = form.save(commit=False)
+            question.create_date = timezone.now()
+            question.save()
+            return redirect('board:index')
+    else:
+        form = QuestionForm()
+
+    context = {'form': form}
+
+    return render(request, 'board/question_form.html', context)
--- config/settings.py
+++ config/settings.py
@@ -68,7 +68,10 @@ ROOT_URLCONF = 'config.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            # BASE_DIR / 'templates'
+            os.path.join(BASE_DIR, 'board/templates')
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -132,3 +135,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+    # BASE_DIR / 'static',
+    os.path.join(BASE_DIR, 'static')
+]
