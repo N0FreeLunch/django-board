b11c5ee60452d3a440e3208071008bfcb39d6b48
N0FreeLunch <consilience90@gmail.com>
2021-01-13 21:01:23 +0900

    use form and use bootstrap

--- board/views.py
+++ board/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, get_object_or_404, redirect
 from django.http import HttpResponse
 from .models import Question
 from django.utils import timezone
-from .forms import QuestionForm
+from .forms import QuestionForm, AnswerForm
 
 # Create your views here.
 def index(request):
@@ -25,8 +25,20 @@ def answer_create(request, question_id):
     pybo 답변등록
     """
     question = get_object_or_404(Question, pk=question_id)
-    question.answer_set.create(content=request.POST.get('content'), create_date=timezone.now())
-    return redirect('board:detail', question_id=question.id)
+    # question.answer_set.create(content=request.POST.get('content'), create_date=timezone.now())
+    if request.method == "POST":
+        form = AnswerForm(request.POST)
+        if form.is_valid():
+            answer = form.save(commit=False)
+            answer.create_date = timezone.now()
+            answer.question = question
+            answer.save()
+            return redirect('board:detail', question_id=question.id)
+    else:
+        form = AnswerForm()
+    context = {'question': question, 'form': form}
+    return render(request, 'board/question_detail.html', context)
+    # return redirect('board:detail', question_id=question.id)
 
 def question_create(request):
     """
