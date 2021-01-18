1660116291dd266abfbb426c04b5ce50015fc48f
N0FreeLunch <consilience90@gmail.com>
2021-01-18 22:04:58 +0900

    question and answert modify added

--- board/views.py
+++ board/views.py
@@ -5,6 +5,7 @@ from django.utils import timezone
 from .forms import QuestionForm, AnswerForm
 from django.core.paginator import Paginator
 from django.contrib.auth.decorators import login_required
+from django.contrib import messages
 
 
 # Create your views here.
@@ -75,4 +76,25 @@ def question_create(request):
 
     context = {'form': form}
 
+    return render(request, 'board/question_form.html', context)
+
+@login_required(login_url='common:login')
+def question_modify(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    print(question.__dict__)
+    if request.user != question.author:
+        messages.error(request, '수정권한이 없습니다')
+        return redirect('board:detail', question_id=question.id)
+
+    if request.method == "POST":
+        form = QuestionForm(request.POST, instance=question)
+        if form.is_valid():
+            question = form.save(commit=False)
+            question.author = request.user
+            question.modify_date = timezone.now()  # 수정일시 저장
+            question.save()
+            return redirect('board:detail', question_id=question.id)
+    else:
+        form = QuestionForm(instance=question)
+    context = {'form': form}
     return render(request, 'board/question_form.html', context)
