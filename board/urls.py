8d0787532e371f58ce08780872d739b8ec06c72c
N0FreeLunch <consilience90@gmail.com>
2021-01-26 23:30:21 +0900

    recommend added

--- board/urls.py
+++ board/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 # from . import views
-from .views import base_views, question_views, answer_views, comment_views
+from .views import base_views, question_views, answer_views, comment_views, vote_views
 
 app_name = 'board'
 
@@ -55,4 +55,6 @@ urlpatterns = [
          comment_views.comment_modify_answer, name='comment_modify_answer'),
     path('comment/delete/answer/<int:comment_id>/',
          comment_views.comment_delete_answer, name='comment_delete_answer'),
+
+    path('vote/question/<int:question_id>/', vote_views.vote_question, name='vote_question'),
 ]
