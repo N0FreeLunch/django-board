32b42df47d2fb83201833b83f7afcee47d7fb58e
N0FreeLunch <consilience90@gmail.com>
2021-02-06 15:58:24 +0900

    debug mode setting

--- config/settings.py
+++ config/settings.py
@@ -36,7 +36,7 @@ def get_secret(setting, secrets=secrets):
 SECRET_KEY = get_secret("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = get_secret("debug")
 
 ALLOWED_HOSTS = get_secret("domains")
 
