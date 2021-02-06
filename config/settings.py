a8a2af60bcfd5e17ee65471be2cb5a8446fcfe64
N0FreeLunch <consilience90@gmail.com>
2021-02-06 15:23:43 +0900

    server static connection

--- config/settings.py
+++ config/settings.py
@@ -137,10 +137,14 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATICFILES_DIRS = [
-    # BASE_DIR / 'static',
-    os.path.join(BASE_DIR, 'static')
-]
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATICFILES_DIRS = []
+
+# STATICFILES_DIRS = [
+#     # BASE_DIR / 'static',
+#     os.path.join(BASE_DIR, 'static')
+# ]
 
 LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/'
+
